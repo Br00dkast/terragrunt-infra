7d06c6fedd012c627206d99bb8788f157fb9340f
ansible <ansible@dvldr.me>
2025-11-08 21:09:49 +0000

    Revert "fix: setup ci"
    This reverts commit 2e50695c8b96d00b79b9e916ad8203e74e88b97b.

--- proxmox/mgmt/lxc/imports.tf
+++
@@ -1,9 +0,0 @@
-# This import block targets a specific instance within the for_each loop
-# defined in your main resource block.
-import {
-  # The resource address now includes the key from your lxc_containers map.
-   to = proxmox_virtual_environment_container.this["lxc-mgmt-netbird"]
-
-  # The ID of the existing LXC container in Proxmox.
-  id = "pve-node02/111"
-}
